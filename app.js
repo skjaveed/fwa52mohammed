f1fcfa00da63ae50f20bb5e4e1f28561fb8eeafa
javeed shaik <39088317+skjaveed@users.noreply.github.com>
2021-10-27 23:07:21 -0500

    Added Users

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mydataRouter = require('./routes/mydata');
+var usersRouter = require('./routes/users')
+var mydataRouter = require('./routes/mydata')
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);
+app.use('/users', usersRouter)
+app.use('/mydata', mydataRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
